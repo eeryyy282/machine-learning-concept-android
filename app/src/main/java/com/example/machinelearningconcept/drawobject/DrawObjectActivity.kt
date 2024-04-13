077212bdddbb467935b15258d033a7733ee4ef3c
eeryyy282 <airiagustus82@gmail.com>
2024-04-14 01:17:48 +0700

    add activity for draw object

--- app/src/main/java/com/example/machinelearningconcept/drawobject/DrawObjectActivity.kt
+++ app/src/main/java/com/example/machinelearningconcept/drawobject/DrawObjectActivity.kt
@@ -1,31 +1,21 @@
-package com.example.machinelearningconcept
+package com.example.machinelearningconcept.drawobject
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.machinelearningconcept.databinding.ActivityMainBinding
-import com.example.machinelearningconcept.drawobject.DrawObjectActivity
+import com.example.machinelearningconcept.R
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class DrawObjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_draw_object)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding.drawObjectButton.setOnClickListener {
-            val moveToDrawActivity = Intent(this@MainActivity, DrawObjectActivity::class.java)
-            startActivity(moveToDrawActivity)
-        }
-
     }
 }
